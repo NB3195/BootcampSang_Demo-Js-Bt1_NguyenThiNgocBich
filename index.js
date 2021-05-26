46452c9a748d25f00d003d9ebbc93ad379310cc1
Bich <>
2021-05-26 21:58:17 +0700

    up load bt

---
+++ index.js
@@ -0,0 +1,21 @@
+// Bài tập 1
+/**
+ * Yêu cầu
+ * Viết chương trình tính tiền lương nhân viên :
+ * Lương 1 ngày : 100.000
+ * Yêu cầu người dùng nhập vào số ngày làm.
+ * Công thức tính lương : lương 1 ngày * số ngày làm.
+ */
+/**
+ * Phân Tích
+ * Input : 1 ngày (100.000)
+ *Giai thuật : 
+ - Người dùng nhập vào số ngày làm .
+ - Tiền Lương : lương 1 ngày (100.000) * Số ngày làm.
+ * Output : Hiển thị ra màn hình console.
+ */
+// Bài làm
+var luongMotNgay = 100000;
+var soNgayLamCuaNguoiDungNhapVao = 15;
+var tienLuongCuaNguoiDung = 100000 * 15;
+console.log("Tiền Lương Người Dùng là : ", tienLuongCuaNguoiDung);
